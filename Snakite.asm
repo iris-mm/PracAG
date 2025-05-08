8fbeae0636c23739bdbafa52653615314b7c5f2b
Lumi151 <l.lorente.2023@alumnos.urjc.es>
2025-05-08 15:06:40 +0200

    Dibujo de personaje

--- Snakite.asm
+++ Snakite.asm
@@ -15,15 +15,37 @@ y_k: .word 8
 main: 
 la $t0, frameBuffer
 lw $t1, color_fondo
-li $t2, 65536		  	# Esto es el número de píxeles a pintar del fondo (256x256)
+lw $t3, color_kite
+
+main_loop:
+li $t2, 256		  	# Esto es el número de píxeles a pintar del fondo (16x16)
 
 colorear_fondo:
+
 sw $t1, 0($t0)            	# Guardar el color en la dirección actual
 addi $t0, $t0, 4          	# Mover a la siguiente posición de píxel
 addi $t2, $t2, -1         	# Decrementar contador
 bgtz $t2, colorear_fondo    	# Repetir mientras queden píxeles
 
-dibujar_personaje
+
+dibujar_personaje:
+la $t0, frameBuffer
+				#Todo esto calcula el n�mero de celda a partir de las coordenadas
+lw $t4, x_k
+lw $t5, y_k
+li $t9, 16
+mult  $t5, $t9
+mflo $t5
+add $t5, $t5, $t4
+
+				#Calcula el offset correspondiente a dicho n�mero de celda
+li $t9, 4
+mult $t5, $t9
+mflo $t6
+add $t7, $t0, $t6
+
+				#Guarda el color del personaje en la direcci�n de memoria correspondiente
+sw $t3, 0($t7)
 
 end:
 j end
